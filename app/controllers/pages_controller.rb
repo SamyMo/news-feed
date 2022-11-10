ee3f2299ae160ae54561a757b982fa5492cd9dec
Samy Mokrane <samy.mokrane@gmail.com>
2022-11-10 10:16:19 +0100

    search fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -22,6 +22,20 @@ class PagesController < ApplicationController
 
 private
 
-  def find_country
+  def request_api(url)
+    response = Excon.get(
+      url,
+      headers: {
+        'X-RapidAPI-Host' => URI.parse(url).host,
+        'X-RapidAPI-Key' => ENV.fetch('da39ecad288a4d8782dd37d3771880fa')
+      }
+    )
+    return nil if response.status != 200
+    JSON.parse(response.body)
+  end
+  def find_country(name)
+    request_api(
+      "https://newsapi.org/v2/top-headlines?&/#{URI.encode(name)}"
+    )
   end
 end
