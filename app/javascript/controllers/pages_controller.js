1a21ea8c5d4e9cc2f937df509561ccbb583777c3
Samy Mokrane <samy.mokrane@gmail.com>
2022-11-17 11:07:05 +0100

    add fetch promsie to js controller

--- app/javascript/controllers/pages_controller.js
+++ app/javascript/controllers/pages_controller.js
@@ -20,3 +20,19 @@
       });
 }
 
+
+
+const myInfo = document.getElementsByTagName(p)
+
+
+let promise = fetch("https:https://newsapi.org/v2/top-headlines?country=fr&category=general&apiKey=da39ecad288a4d8782dd37d3771880fa")
+.then(response => response.json())
+.then(response => alert(JSON.stringify(response)))
+.then((data) => {
+
+})
+.then(function(myBlob) {
+  const objectURL = URL.createObjectURL(myInfo);
+  myImage.src = objectURL;
+}
+.catch(error => alert("Erreur : " + error));
